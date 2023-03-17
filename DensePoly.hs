0951f12bbdc101fe6c25fe459de56c837723de59
Anna Stawiska <ania.stawiska@gmail.com>
2023-03-17 12:35:48 +0100

    Implemented (+), negate, abs, signum in Num(DensePoly a)

--- DensePoly.hs
+++ DensePoly.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs #-}
 module DensePoly() where
 import PolyClass
 import Representation
@@ -8,6 +9,30 @@ instance Polynomial DensePoly where
 
 instance (Eq a, Num a) => Num (DensePoly a) where
 
+    abs :: (Eq a, Num a) => DensePoly a -> DensePoly a
+    abs x = undefined
+
+    signum :: (Eq a, Num a) => DensePoly a -> DensePoly a
+    signum x = undefined
+
+    -- TODO: fromInteger
+    -- fromInteger :: Integer -> DensePoly a
+
+    (+) :: (Eq a, Num a) => DensePoly a -> DensePoly a -> DensePoly a
+    (+) x y = P $ reverse $ dropWhile (0 ==) (go [] (unP x) (unP y)) where
+        go list (x:xs) (y:ys) = go (x+y:list) xs ys
+        go list [] (y:ys) = go (y:list) [] ys
+        go list (x:xs) [] = go (x:list) xs []
+        go list [] [] = list
+        
+    -- TODO: (-)
+    -- (*) :: (Eq a, Num a) => DensePoly a -> DensePoly a -> DensePoly a
+
+    negate :: (Eq a, Num a) => DensePoly a -> DensePoly a
+    negate x = P $ reverse $ dropWhile (0 ==) (go [] (unP x)) where
+        go list (x:xs) = go (-x:list) xs
+        go list [] = list
+
 -- |
 -- >>> x^3 - 1 :: DensePoly Integer 
 -- P {unP = [-1,0,0,1]}
@@ -16,8 +41,7 @@ instance (Eq a, Num a) => Num (DensePoly a) where
 -- >>> isCanonicalDP (sampleDP - sampleDP)
 -- True
     
-instance (Eq a, Num a) => Eq (DensePoly a) where
-    
+instance (Eq a, Num a) => Eq (DensePoly a) where    
 
 -- |
 -- >>>  P [1,2] == P [1,2]
