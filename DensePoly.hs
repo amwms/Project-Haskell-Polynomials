df570d193a55dd26afd837411d5cf0780f4f7aa9
Anna Stawiska <ania.stawiska@gmail.com>
2023-03-23 16:33:49 +0100

    Implemented (*) and updated negate and style fixes

--- DensePoly.hs
+++ DensePoly.hs
@@ -3,6 +3,12 @@ module DensePoly() where
 import PolyClass
 import Representation
 
+toCanonical :: (Eq a, Num a) => [a] -> [a]
+toCanonical xs = reverse $ dropWhile (0 ==) (reverse xs)
+
+reverseToCanonical :: (Eq a, Num a) => [a] -> [a]
+reverseToCanonical xs = reverse $ dropWhile (0 ==) xs
+
 instance Functor DensePoly where
     fmap :: (a -> b) -> DensePoly a -> DensePoly b
     fmap fun (P xs) = P $ map fun xs
@@ -21,10 +27,10 @@ instance Polynomial DensePoly where
     evalP  :: Num a => DensePoly a -> a -> a        -- value of p(x) at given x
     evalP (P xs) x = go xs 0 where
         go [] _ = 0
-        go (h:xs) n = h * (x ^ n) + go xs (n + 1)
+        go (h : xs) n = h * (x ^ n) + go xs (n + 1)
 
     shiftP :: (Eq a, Num a) => Int -> DensePoly a -> DensePoly a -- multiply by x^n
-    shiftP n (P xs) = P $ replicate n 0 ++ reverse (dropWhile (0 ==) (reverse xs))
+    shiftP n (P xs) = P $ replicate n 0 ++ toCanonical xs
 
     degree :: (Eq a, Num a) => DensePoly a -> Int -- highest power with nonzero coefficient
     degree (P xs) = length (dropWhile (0 ==) (reverse xs)) - 1
@@ -43,24 +49,24 @@ instance (Eq a, Num a) => Num (DensePoly a) where
         | otherwise = P [fromInteger x]
 
     (+) :: (Eq a, Num a) => DensePoly a -> DensePoly a -> DensePoly a
-    (+) x y = P $ reverse 
-                    $ dropWhile (0 ==) (go [] (unP x) (unP y)) where
-                        go list (x:xs) (y:ys) = go (x+y:list) xs ys
-                        go list [] (y:ys) = go (y:list) [] ys
-                        go list (x:xs) [] = go (x:list) xs []
-                        go list [] [] = list
+    (+) x y = P $ reverseToCanonical (go [] (unP x) (unP y)) where
+        go list [] [] = list
+        go list (x : xs) [] = go (x : list) xs []
+        go list [] (y : ys) = go (y : list) [] ys
+        go list (x : xs) (y : ys) = go (x + y : list) xs ys
 
-    -- TODO (*)
-    -- (*) :: (Eq a, Num a) => DensePoly a -> DensePoly a -> DensePoly a
-    -- (*) x y = go (unP x) (unP y) where
-    --     go [] _  = P []
-    --     go (x : xs) ys =  P $ reverse $ dropWith (0 ==) reverse (unP (P ((map (*x) ys))) + (P ((0 : (go xs ys)))))
+    -- -- TODO (*)
+    (*) :: (Eq a, Num a) => DensePoly a -> DensePoly a -> DensePoly a
+    (*) x y = go (unP x) (unP y) where
+        go [] _  = P []
+        go (x:xs) ys =  P $ toCanonical (unP $ (P ((map (*x) ys))) + (P ((0 : unP (go xs ys)))))
+
 
     negate :: (Eq a, Num a) => DensePoly a -> DensePoly a
-    negate x = P $ reverse 
-                    $ dropWhile (0 ==) (go [] (unP x)) where
-                        go list (x:xs) = go (-x:list) xs
-                        go list [] = list
+    negate x = P $ toCanonical $ map (* (-1)) (unP x)
+    -- negate x = P $ reverseToCanonical (go [] (unP x)) where
+    --     go list [] = list
+    --     go list (x : xs) = go (-x : list) xs
 
 -- |
 -- >>> x^3 - 1 :: DensePoly Integer 
