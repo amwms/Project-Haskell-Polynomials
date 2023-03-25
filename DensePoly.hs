c711fa56745ba59e572a6b4f1c0af2d0241c7baa
Anna Stawiska <ania.stawiska@gmail.com>
2023-03-25 17:06:58 +0100

    Updated to have function signatures in comments

--- DensePoly.hs
+++ DensePoly.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE InstanceSigs #-}
 module DensePoly() where
 import PolyClass
 import Representation
@@ -10,59 +9,57 @@ reverseToCanonical :: (Eq a, Num a) => [a] -> [a]
 reverseToCanonical xs = reverse $ dropWhile (0 ==) xs
 
 instance Functor DensePoly where
-    fmap :: (a -> b) -> DensePoly a -> DensePoly b
+    -- fmap :: (a -> b) -> DensePoly a -> DensePoly b
     fmap fun (P xs) = P $ map fun xs
 
 instance Polynomial DensePoly where
-
-    zeroP :: DensePoly a
+    -- zeroP :: DensePoly a
     zeroP = P []
 
-    constP :: (Eq a, Num a) => a -> DensePoly a
+    -- constP :: (Eq a, Num a) => a -> DensePoly a
     constP x 
         | x == 0 = P []
         | otherwise = P [x]
 
-    varP   :: Num a => DensePoly a                 
+    -- varP   :: Num a => DensePoly a                 
     varP = P [0, 1]
 
-    evalP  :: Num a => DensePoly a -> a -> a        
+    -- evalP  :: Num a => DensePoly a -> a -> a        
     evalP (P xs) x = go xs 0 where
         go [] _ = 0
         go (h : xs) n = h * (x ^ n) + go xs (n + 1)
 
-    shiftP :: (Eq a, Num a) => Int -> DensePoly a -> DensePoly a 
+    -- shiftP :: (Eq a, Num a) => Int -> DensePoly a -> DensePoly a 
     shiftP n (P xs) = P $ toCanonical $ replicate n 0 ++ xs
 
-    degree :: (Eq a, Num a) => DensePoly a -> Int 
+    -- degree :: (Eq a, Num a) => DensePoly a -> Int 
     degree (P xs) = length (dropWhile (0 ==) (reverse xs)) - 1
 
 instance (Eq a, Num a) => Num (DensePoly a) where
-
-    abs :: (Eq a, Num a) => DensePoly a -> DensePoly a
+    -- abs :: (Eq a, Num a) => DensePoly a -> DensePoly a
     abs x = undefined
 
-    signum :: (Eq a, Num a) => DensePoly a -> DensePoly a
+    -- signum :: (Eq a, Num a) => DensePoly a -> DensePoly a
     signum x = undefined
 
-    fromInteger :: Integer -> DensePoly a
+    -- fromInteger :: Integer -> DensePoly a
     fromInteger x
         | x == 0 = P []
         | otherwise = P [fromInteger x]
 
-    (+) :: (Eq a, Num a) => DensePoly a -> DensePoly a -> DensePoly a
+    -- (+) :: (Eq a, Num a) => DensePoly a -> DensePoly a -> DensePoly a
     (+) x y = P $ reverseToCanonical (go [] (unP x) (unP y)) where
         go list [] [] = list
         go list (x : xs) [] = go (x : list) xs []
         go list [] (y : ys) = go (y : list) [] ys
         go list (x : xs) (y : ys) = go (x + y : list) xs ys
 
-    (*) :: (Eq a, Num a) => DensePoly a -> DensePoly a -> DensePoly a
+    -- (*) :: (Eq a, Num a) => DensePoly a -> DensePoly a -> DensePoly a
     (*) x y = go (unP x) (unP y) where
         go [] _  = P []
         go (x:xs) ys =  P $ toCanonical (unP $ (P ((map (*x) ys))) + (P ((0 : unP (go xs ys)))))
 
-    negate :: (Eq a, Num a) => DensePoly a -> DensePoly a
+    -- negate :: (Eq a, Num a) => DensePoly a -> DensePoly a
     negate x = P $ toCanonical $ map (* (-1)) (unP x)
 
 -- |
@@ -74,10 +71,10 @@ instance (Eq a, Num a) => Num (DensePoly a) where
 -- True
 
 instance (Eq a, Num a) => Eq (DensePoly a) where
-    (==) :: DensePoly a -> DensePoly a -> Bool
+    -- (==) :: DensePoly a -> DensePoly a -> Bool
     (==) x y = degree x == degree y && dropWhile (0 ==) (reverse (unP x)) ==  dropWhile (0 ==) (reverse (unP y))
 
-    (/=) :: DensePoly a -> DensePoly a -> Bool
+    -- (/=) :: DensePoly a -> DensePoly a -> Bool
     (/=) x y = degree x /= degree y || dropWhile (0 ==) (reverse (unP x)) /= dropWhile (0 ==) (reverse (unP y))
 
 -- |
--- SparsePoly.hs
+++ SparsePoly.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE InstanceSigs #-}
 module SparsePoly(fromDP, toDP, qrP) where
 import PolyClass
 import Representation
@@ -40,64 +39,63 @@ second :: (b -> b') -> (a, b) -> (a, b')
 second f (x, y) = (x, f y)
 
 instance Functor SparsePoly where
-    fmap :: (a -> b) -> SparsePoly a -> SparsePoly b
+    -- fmap :: (a -> b) -> SparsePoly a -> SparsePoly b
     fmap fun (S xs) = S $ map (second fun) xs
 
 instance Polynomial SparsePoly where
-    zeroP :: SparsePoly a
+    -- zeroP :: SparsePoly a
     zeroP = S []
 
-    constP :: (Eq a, Num a) => a -> SparsePoly a
+    -- constP :: (Eq a, Num a) => a -> SparsePoly a
     constP x
         | x == 0 = S []
         | otherwise = S [(0, x)]
 
-    varP   :: Num a => SparsePoly a                 
+    -- varP   :: Num a => SparsePoly a                 
     varP = S [(1, 1)]
 
-    evalP :: Num a => SparsePoly a -> a -> a        
+    -- evalP :: Num a => SparsePoly a -> a -> a        
     evalP (S xs) x = go xs where
         go [] = 0
         go (h : xs) = pairSecond h * (x ^ pairFirst h) + go xs
 
-    shiftP :: (Eq a, Num a) => Int -> SparsePoly a -> SparsePoly a 
+    -- shiftP :: (Eq a, Num a) => Int -> SparsePoly a -> SparsePoly a 
     shiftP n (S xs) = S $ map (first (+n)) xs
 
-    degree :: (Eq a, Num a) => SparsePoly a -> Int 
+    -- degree :: (Eq a, Num a) => SparsePoly a -> Int 
     degree (S xs) = case xs of
         [] -> -1
         x : _ -> pairFirst x
 
 instance (Eq a, Num a) => Num (SparsePoly a) where
-
-    abs :: (Eq a, Num a) => SparsePoly a -> SparsePoly a
+    -- abs :: (Eq a, Num a) => SparsePoly a -> SparsePoly a
     abs x = undefined
 
-    signum :: (Eq a, Num a) => SparsePoly a -> SparsePoly a
+    -- signum :: (Eq a, Num a) => SparsePoly a -> SparsePoly a
     signum x = undefined
 
-    fromInteger :: (Eq a, Num a) => Integer -> SparsePoly a
+    -- fromInteger :: (Eq a, Num a) => Integer -> SparsePoly a
     fromInteger x
         | x == 0 = S []
         | otherwise = S [(0, fromInteger x)]
 
-    (+) :: (Eq a, Num a) => SparsePoly a -> SparsePoly a -> SparsePoly a
+    -- (+) :: (Eq a, Num a) => SparsePoly a -> SparsePoly a -> SparsePoly a
     (+) x y = S $ listToCanonical $ unS x ++ unS y
 
-    (*) :: (Eq a, Num a) => SparsePoly a -> SparsePoly a -> SparsePoly a
+    -- (*) :: (Eq a, Num a) => SparsePoly a -> SparsePoly a -> SparsePoly a
     (*) x y = S $ listToCanonical [(exp1 + exp2, coef1 * coef2) | (exp1, coef1) <- unS x, (exp2, coef2) <- unS y]
 
-    negate :: (Eq a, Num a) => SparsePoly a -> SparsePoly a
+    -- negate :: (Eq a, Num a) => SparsePoly a -> SparsePoly a
     negate x = S $ listToCanonical $ map (\x -> (pairFirst x, negate (pairSecond x))) (unS x)
 
-    (-) :: (Eq a, Num a) => SparsePoly a -> SparsePoly a -> SparsePoly a
+    -- (-) :: (Eq a, Num a) => SparsePoly a -> SparsePoly a -> SparsePoly a
     (-) x y = x + negate y
 
 instance (Eq a, Num a) => Eq (SparsePoly a) where
-    (==) :: (Eq a, Num a) => SparsePoly a -> SparsePoly a -> Bool
+    -- (==) :: (Eq a, Num a) => SparsePoly a -> SparsePoly a -> Bool
     p == q = nullP(p-q)
 
-    (/=) :: (Eq a, Num a) => SparsePoly a -> SparsePoly a -> Bool
+    -- (/=) :: (Eq a, Num a) => SparsePoly a -> SparsePoly a -> Bool
     p /= q = not (nullP(p-q))
 
 qrP :: (Eq a, Fractional a) => SparsePoly a -> SparsePoly a -> (SparsePoly a, SparsePoly a)
